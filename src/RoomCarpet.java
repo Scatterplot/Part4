e52d0c5b29245b4b0ac88046895ad8db481090c6
Nathan Bingham <scatterplot@outlook.com>
2015-01-28 19:28:07 -0800

    Test update

--- src/RoomCarpet.java
+++ src/RoomCarpet.java
@@ -1,5 +1,20 @@
 /**
- * Created by Development on 1/28/15.
+ * Created by Nathan Bingham on 1/28/15.
  */
-public class RoomCarpet {
+
+public class RoomCarpet
+{
+    private RoomDimension dimension;
+    private double carpetCost;
+
+    public RoomCarpet(RoomDimension dimension, double cost)
+    {
+        this.dimension = dimension;
+        this.carpetCost = cost;
+    }
+
+    public double getTotalCost()
+    {
+        return this.carpetCost * this.dimension.getArea();
+    }
 }
--- src/RoomDimension.java
+++ src/RoomDimension.java
@@ -1,5 +1,25 @@
 /**
- * Created by Development on 1/28/15.
+ * Created by Nathan Bingham on 1/28/15.
  */
-public class RoomDimension {
+
+public class RoomDimension
+{
+    private double length;
+    private double width;
+
+    public RoomDimension(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea()
+    {
+        return this.length * this.width;
+    }
+
+    public String toString()
+    {
+        return "L:"+length+",W:"+width;
+    }
 }
